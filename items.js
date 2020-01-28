a8318bafc438ec7a610f15793a6cd09dc4710b5e
Cody Brown <cody6lefever@gmail.com>
2020-01-27 16:43:00 -0800

    added item list

---
+++ items.js
@@ -0,0 +1,145 @@
+//creat some items based on the pics and give them id, image, name
+const bag = {
+    id: '',
+    image: 'bag.jpg',
+    name: Bag
+};
+
+const banana = {
+    id: '',
+    image: 'banana.jpg',
+    name: Banana
+};
+
+const bathroom = {
+    id: '',
+    image: 'bathroom.jpg',
+    name: Bathroom
+};
+
+const boots = {
+    id: '',
+    image: 'boots.jpg',
+    name: Boots
+};
+
+const breakfast = {
+    id: '',
+    image: 'breakfast.jpg',
+    name: Breakfast
+};
+
+const bubblegum = {
+    id: '',
+    image: 'bubblegum.jpg',
+    name: Bubblegum
+};
+
+const chair = {
+    id: '',
+    image: 'chair.jpg',
+    name: Chair
+};
+
+const cthulhu = {
+    id: '',
+    image: 'cthulhu.jpg',
+    name: Cthulhu
+};
+
+const dogDuck = {
+    id: '',
+    image: 'dog-duck.jpg',
+    name: Dog Duck
+};
+
+const dragon = {
+    id: '',
+    image: 'dragon.jpg',
+    name: Dragon
+};
+
+const pen = {
+    id: '',
+    image: 'pen.jpg',
+    name: Pen
+};
+
+const petSweep = {
+    id: '',
+    image: 'pet-sweep.jpg',
+    name: Pet Sweep
+};
+
+const scissors = {
+    id: '',
+    image: 'scissors.jpg',
+    name: Scissors
+};
+
+const shark = {
+    id: '',
+    image: 'shark.jpg',
+    name: Shark
+};
+
+const sweep = {
+    id: '',
+    image: 'sweep.jpg',
+    name: sweep
+};
+
+const tauntaun = {
+    id: '',
+    image: 'tauntaun.jpg',
+    name: Tuantuan
+};
+
+const unicorn = {
+    id: '',
+    image: 'unicorn.jpg',
+    name: unicorn,
+};
+
+const usb = {
+    id: '',
+    image: 'usb.jpg',
+    name: usb,
+};
+
+const waterCan  = {
+    id: '',
+    image: 'water-can.jpg',
+    name: Water Can
+};
+
+const wineGlass = {
+    id: '',
+    image: 'wine-glass.jpg',
+    name: Wine Glass
+};
+
+const items = [
+    bag,
+    banana,
+    bathroom,
+    boots,
+    breakfast,
+    bubblegum,
+    chair,
+    cthulhu,
+    dogDuck,
+    dragon,
+    pen,
+    petSweep,
+    scissors,
+    shark,
+    sweep,
+    tauntaun,
+    unicorn,
+    usb,
+    waterCan,
+    wineGlass
+];
+
+export default items;
