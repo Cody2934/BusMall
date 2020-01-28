0dad3b7189d7a6ed4049fead720ccf6a794aea82
Cody Brown <cody6lefever@gmail.com>
2020-01-28 12:30:39 -0800

    changed to get element by id

---
+++ app.js
@@ -0,0 +1,62 @@
+import { productsData } from './productsData.js';
+import { productsArray } from './productsArray.js';
+
+const resultDisplay = document.getElementById('results');
+const submitButton = document.getElementById('submitButton');
+const product3Input = document.getElementById('product3Input');
+const products = new productsArray(productsData);
+
+//let productResults = [];
+
+let chosenProducts = 0;
+
+const initializeNewProducts = () => {
+    const newProduct1 = products.getRandomProduct();
+    let newProduct2 = products.getRandomProduct();
+    console.log(newProduct1);
+
+    while (newProduct1.id === newProduct2.id){
+        newProduct2 = products.getRandomProduct;
+    }
+    console.log(newProduct2);
+
+    let newProduct3 = products.getRandomProduct();
+    while (newProduct1.id === newProduct3.id || newProduct2.id === newProduct3.id){
+        newProduct3 = products.getRandomProduct();
+    }
+    console.log(newProduct3);
+
+    const product1Object = newProduct1;
+    product1Object.textContent = product1Object.name;
+    const product1Image = document.getElementById('product1Image');
+    product1Image.src = product1Object.image;
+    const product1Name = document.getElementById('product1Name');
+    product1Name.textContent = product1Object.name;
+
+    const product2Object = newProduct2;
+    product2Object.textContent = product2Object.name;
+    const product2Image = document.getElementById('product2Image');
+    product2Image.src = product2Object.image;
+    const product2Name = document.getElementById('product2Name');
+    product2Name.textContent = product2Object.name;
+
+    const product3Object = newProduct3;
+    product3Object.textContent = product3Object.name;
+    const product3Image = document.getElementById('product3Image');
+    product3Image.src = product3Object.image;
+    const product3Name = document.getElementById('product3Name');
+    product3Name.textContent = product3Object.name;
+};
+
+initializeNewProducts();
+
+submitButton.addEventListener('click', () => {
+    console.log(document.querySelector('input:checked'));
+    initializeNewProducts();
+    chosenProducts++;
+    if (chosenProducts > 25){
+        resultDisplay.style.visibility = 'visible';
+        submitButton.disabled = 'true';
+    }
+});
+
---
+++ productsData.js
@@ -0,0 +1,104 @@
+//creat some products based on the pics and give them id, image, name
+export const productsData = [
+    {
+        id: 'bag',
+        image: 'bag.jpg',
+        name: 'Bag'
+    },
+    {
+        id: 'banana',
+        image: 'banana.jpg',
+        name: 'Banana'
+    },
+    {
+        id: 'bathroom',
+        image: 'bathroom.jpg',
+        name: 'Bathroom'
+    },
+    {
+        id: 'boots',
+        image: 'boots.jpg',
+        name: 'Boots'
+    },
+    {
+        id: 'breakfast',
+        image: 'breakfast.jpg',
+        name: 'Breakfast'
+    },
+    {
+        id: 'bubblegum',
+        image: 'bubblegum.jpg',
+        name: 'Bubblegum'
+    },
+    {
+        id: 'chair',
+        image: 'chair.jpg',
+        name: 'Chair'
+    },
+    {
+        id: 'cthulu',
+        image: 'cthulhu.jpg',
+        name: 'Cthulhu'
+    },
+    {
+        id: 'dogDuck',
+        image: 'dog-duck.jpg',
+        name: 'Dog Duck'
+    },
+    {
+        id: 'dragon',
+        image: 'dragon.jpg',
+        name: 'Dragon'
+    },
+    {
+        id: 'pen',
+        image: 'pen.jpg',
+        name: 'Pen'
+    },
+    {
+        id: 'petSweep',
+        image: 'pet-sweep.jpg',
+        name: 'Pet Sweep'
+    },
+    {
+        id: 'scissors',
+        image: 'scissors.jpg',
+        name: 'Scissors'
+    },
+    {
+        id: 'shark',
+        image: 'shark.jpg',
+        name: 'Shark'
+    },
+    {
+        id: 'sweep',
+        image: 'sweep.jpg',
+        name: 'Sweep'
+    },
+    {
+        id: 'tauntaun',
+        image: 'tauntaun.jpg',
+        name: 'Tuantuan'
+    },
+    {
+        id: 'unicorn',
+        image: 'unicorn.jpg',
+        name: 'Unicorn'
+    },
+    {
+        id: 'usb',
+        image: 'usb.jpg',
+        name: 'USB'
+    },
+    {
+        id: 'waterCan',
+        image: 'water-can.jpg',
+        name: 'Water Can'
+    },
+    {
+
+        id: 'wineGlass',
+        image: 'wine-glass.jpg',
+        name: 'Wine Glass'
+    },
+];
